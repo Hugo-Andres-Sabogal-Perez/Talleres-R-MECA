ba2fd2fdf53871c753870733b829de3ec6fe1286
HugoSabogal <88988214+Hugo-Andres-Sabogal-Perez@users.noreply.github.com>
2024-11-07 19:03:57 -0500

    avances en text mining

--- Taller 3/Taller3.R
+++ Taller 3/Taller3.R
@@ -3,19 +3,19 @@ rm(list = ls())
 
 #0.1. Librerías -------------------------------------
 
-library(dplyr)
+library(tidyverse)
 library(sf)
 library(openxlsx)
-library(ggplot2)
 library(plotly)
 library(rvest)
 library(tidytext)
 library(rebus)
 library(tidyr)
 
+
 #0.2. Directorio ------------------------------------
 
-setwd("C:/Users/Natalia/OneDrive - Universidad de los Andes/Documentos/2024-2/R/Talleres-R-MECA/Taller 3")
+setwd("C:/Users/hugos/OneDrive - Universidad de los andes/MECA/Semestre 1/taller R/Talleres-R-MECA/Taller 3")
 
 # Punto 1 -------------------------------------------
 
@@ -44,7 +44,7 @@ df_completo <- shapefile_col %>% left_join(panel_cede, by = "codmpio")
 datos_mapa <- subset(df_completo, df_completo$ano == 2009)
 
 mapa <- ggplot(data = datos_mapa) +
-  geom_sf(aes(fill = pobl_rur), color = "white", size = 0.2) +  # Color y borde de los municipios
+  geom_sf(aes(fill = pobl_rur), size = 0.1) +  # Color y borde de los municipios
   scale_fill_viridis_c(name = "Población Rural", option = "viridis", labels = scales::comma) +  # Escala de color
   labs(title = "Mapa de Población Rural por Municipio (2009)") +
   theme_minimal() +
@@ -52,9 +52,13 @@ mapa <- ggplot(data = datos_mapa) +
     plot.title = element_text(size = 12, face = "bold"),
     legend.position = "bottom")
 
+
+
+mapa
+
 mapa_interactivo <- ggplotly(mapa)
 
-mapa_interactivo
+mapa_interactivo 
 
 # Punto 2 --------------------------------------------
 
@@ -93,5 +97,39 @@ for(page in 1:50) {
   #Registro de progreso
   print(paste("Pagina:", page))
 }
+
+##2.3. remover stop words, minusculas, eliminación de caracteres especiales y numeros -------
+
+corpus <- Corpus(VectorSource(libros_df$Titulo))
+
+# procesamos el texto 
+corpus <- tm_map(corpus, content_transformer(tolower))  # Convertir a minúsculas
+corpus <- tm_map(corpus, removePunctuation)             # Eliminar puntuación
+corpus <- tm_map(corpus, removeNumbers)                 # Eliminar números
+corpus <- tm_map(corpus, removeWords, stopwords("english"))  # Eliminar palabras vacías (stop words)
+corpus <- tm_map(corpus, stripWhitespace)   
+
+# 2.4 creamos base con texto procesado (datos limpios) ---------------
+datos_limpios <-  data.frame(titulo = sapply(corpus, as.character))
+
+# 2.5  creamos base count -----------------
+count <- data.frame(table(datos_limpios))
+
+# 2.6 nube de palabras ----------------
+wordcloud(
+  words = count$titulo, 
+  freq = count$Freq, 
+  min.freq = 1,            # Minimum frequency to include words
+  max.words = 100,           # Maximum number of words
+  random.order = FALSE,     # Arrange words by frequency
+  colors = brewer.pal(8, "Dark2"),  # Set color palette
+  scale =c(0.6,2)
+)
+
+
+# 2.7 matriz term frecuency
+dtm <- TermDocumentMatrix(corpus)
+# Aca ya lo puedo ver. 
+tdm <- as.matrix(dtm)
 
 
